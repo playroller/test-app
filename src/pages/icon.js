e069c6feebc6e5fc59f9dabdb15161db0fa13a07
playroller <gmailreo17@icloud.com>
2022-07-01 13:22:01 +0900

    add new file  pages/icon.js style/icon.css update  style/App.css Material UI/icon勉強用のページを作成、CSSも移植

---
+++ src/pages/icon.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+import IconButton from '@mui/material/Button';
+
+import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+import './../style/icon.css';
+
+
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  onClick(text) {
+    alert(text);
+  }
+
+  render() {
+    return (
+    <div>
+      <IconButton onClick={() => this.onClick('うしたぷにきあくん笑')}>
+        <ChatIcon />
+      </IconButton>
+      <IconButton onClick={() => this.onClick('うっくっく')}>
+        <MoreVertIcon />
+      </IconButton>
+    </div>)
+  };
+}
