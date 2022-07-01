399b5d434c7ae18bdfd71ed071f8c1c49c982d5b
playroller <gmailreo17@icloud.com>
2022-07-01 13:39:25 +0900

    update  components/link.js 各引数の説明をコメントとして追加

--- src/components/link.js
+++ src/components/link.js
@@ -1,5 +1,10 @@
 import { Link } from "react-router-dom";
 
+/*
+link: ページへのURL
+text: リンクを表示するときの文字列
+fsize: フォントサイズ
+ */
 const link = ({link, text, fsize=20}) => {
   return (
     <Link to={link} style={{fontSize: {fsize}}}>{text}</Link> 
